510b09bcc63720837e629ce49806e6af44247d3b
APAHE <kiselev@gismeteo.ru>
2023-01-14 16:24:19 +0300

    add log

--- ha_conn_factory.go
+++ ha_conn_factory.go
@@ -2,6 +2,7 @@ package pool
 
 import (
 	"errors"
+	"log"
 	"math/rand"
 	"strconv"
 	"strings"
@@ -225,6 +226,7 @@ func (pool *clientPool) getConn(key ...string) (*redis.Client, error) {
 		return alives[pool.rand.Intn(n)].redisCli, nil
 	case PollByRoundRobin:
 		pool.ind = (pool.ind + 1) % n
+		log.Println("addr name: ", alives[pool.ind].redisCli.Options().Addr)
 		return alives[pool.ind].redisCli, nil
 	case PollByWeight:
 		r := pool.rand.Int63n(weightRanges[n-1])
